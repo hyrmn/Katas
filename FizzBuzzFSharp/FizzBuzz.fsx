ef659f23bc21b1a4579e170542659f5c1fb9ce08
Ben Hyrman <ben.hyrman@gmail.com>
2014-10-31 12:59:16 -0400

    should be a broken test

--- FizzBuzzFSharp/FizzBuzz.fsx
+++ FizzBuzzFSharp/FizzBuzz.fsx
@@ -4,14 +4,6 @@
 open NUnit.Framework
 open FsUnit
 
-let fizzbuzz number = 
-    let divisibleBy modulus = (number % modulus) = 0
-    match divisibleBy 3, divisibleBy 5 with
-        | true,  false -> "fizz"
-        | false, true -> "buzz"
-        | true,  true -> "fizzbuzz"
-        | false, false -> sprintf "%d" number
-
 [<TestFixture>] 
 type ``Given that I want to fizzbuzz it all up in this joint`` () =
     [<Test>] member x.``When I fizzbuzz 3 I get fizz back`` () = 1 |> should equal 2;;
